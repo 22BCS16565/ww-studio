abcff58c4ad50940a9e28701d889a09cebc81d51
vivek saini <rubylion227@gmail.com>
2025-08-03 02:16:15 +0530

    new commit for render

--- Backend/server.js
+++ Backend/server.js
@@ -29,3 +29,8 @@ mongoose
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('✅ Server is Live on Render!');
+});
+
